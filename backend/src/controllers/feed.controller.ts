b86dada27be8e975aba68b6a7d9e691152b51f40
Ahmed <ahmad.rakha2007@gmail.com>
2026-08-17 22:15:50 +0300

    edit on feed controller

--- backend/src/controllers/feed.controller.ts
+++ backend/src/controllers/feed.controller.ts
@@ -3,12 +3,12 @@ import getFeedService from "../services/feed/getFeed.ts"
 import getFriendsFeedService from "../services/feed/getFriendsFeed.ts"
 
 
-export const getFeed = async (req: Request<{}, {}, {}, {page?: string, limit?: string}>, res: Response) => {
+export const getFeed = async (req: Request<{}, {}, {}, {page?: string, limit?: string}> & {user?: {userId: string}}, res: Response) => {
 
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
-
-    const posts = await getFeedService(page, limit);
+    const {userId} = req.user!
+    const posts = await getFeedService(userId, page, limit);
     
     res.status(200).json({
         success: true,
